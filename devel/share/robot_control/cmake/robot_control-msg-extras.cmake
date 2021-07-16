8aaf7e2b36c25a9c41aee53f0b296c38f5319c55
Nurlan <35225524+KNurlanZ@users.noreply.github.com>
2021-07-16 12:12:53 +0600

    Delete devel directory

--- devel/share/robot_control/cmake/robot_control-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(robot_control_MESSAGE_FILES "/home/nektiria/rl_ws/src/robot_control/msg/coord.msg;/home/nektiria/rl_ws/src/robot_control/msg/state.msg;/home/nektiria/rl_ws/src/robot_control/msg/accelerometr.msg;/home/nektiria/rl_ws/src/robot_control/msg/tactile.msg;/home/nektiria/rl_ws/src/robot_control/msg/rigid.msg;/home/nektiria/rl_ws/src/robot_control/msg/fsrInput.msg;/home/nektiria/rl_ws/src/robot_control/msg/newtactile.msg;/home/nektiria/rl_ws/src/robot_control/msg/contact.msg")
-set(robot_control_SERVICE_FILES "")
